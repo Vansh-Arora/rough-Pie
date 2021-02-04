3ddf48494a7ac9522bd1833ada44953caa549b96
Vansh <vansharora2929@gmail.com>
2021-02-05 03:12:05 +0530

    LHS

--- LHS.py
+++ LHS.py
@@ -1,14 +1,21 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        ansL = []
-        for i in range(len(nums)):
-            ans = 0
-            curr = nums.count(nums[i])
-            if(nums[i] + 1) in nums:
-                successor = nums.count(nums[i] + 1)
-                ans = curr + successor
-            elif(nums[i] - 1) in nums:
-                predecessor = nums.count(nums[i] - 1)
-                ans = predecessor + curr
-            ansL.append(ans)
-        return max(ansL)
+        lengthArray = [] # Array to store length of different series occuring in a test case
+        nums = sorted(nums) # Sorting list before creating dictionary
+        
+        # Now dictionary will have all sorted elements
+        numsDict = {}   # To count numbers and their occurence
+        # Storing nums in numsDict(dictionary)
+        for i in nums:
+            if i in numsDict:
+                numsDict[i] += 1
+            else:
+                numsDict[i] = 1
+
+        for i in numsDict.keys():
+            length = 0    # To store the length of a series
+            if(i + 1) in numsDict.keys():   # Look only for immediate successors
+                length = numsDict[i] + numsDict[i+1]
+            lengthArray.append(length)
+
+        return max(lengthArray)
