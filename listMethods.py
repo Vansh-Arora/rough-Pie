d80a0f86113a9240910ff811cb5de4eeab4f98a8
Vansh <vansharora2929@gmail.com>
2021-02-06 01:28:31 +0530

    rough

--- listMethods.py
+++ listMethods.py
@@ -1,3 +1,18 @@
 arr = [1,1,1,2,2,4,45,3,2,4,5]
 arr.remove(2)
+#print(arr)
+
+stra = "abcd"
+
+stra = stra + "efgh"
+arr.append(stra)
+#print(arr)
+while "" in arr:
+    arr.remove("")
+print(stra)
 print(arr)
+arr.pop()
+print(arr)
+a = '/'
+if a=='/':
+    print(a)
