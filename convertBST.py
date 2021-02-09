39bcd1f1d4cd8624e051c8be3bfc13dcd1f58e2f
Vansh <vansharora2929@gmail.com>
2021-02-10 04:45:52 +0530

    conver to BST

---
+++ convertBST.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if root.right != None:
+            (self.convertBST(root.right))
+            root.val = root.right.val + root.val
+            print(root.val)
+        if root.left != None:
+            root.left.val = root.val + root.left.val
+            (self.convertBST(root.left))
+        if root.left == None and root.right == None :
+            print(root)
+            return root
+        return root
