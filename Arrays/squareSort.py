d67fcd7ec5a2bf91ddeb1e2f944f324bba471ad8
Vansh <vansharora2929@gmail.com>
2021-02-15 01:37:59 +0530

    solved using merge sort

---
+++ Arrays/squareSort.py
@@ -0,0 +1,37 @@
+class Solution:
+    def mergeSort(self, arr, n):
+    # From here we divide.
+        if n>1:                     # Divide the array in 2 parts until the length of array is 1
+            arr1 = arr[:int(n/2)]
+            arr2 = arr[int(n/2):]
+            self.mergeSort(arr1,len(arr1))
+            self.mergeSort(arr2,len(arr2))
+            # In this pasrt we start conquer.
+            i=0
+            k=0
+            j=0
+            while i<len(arr1) and j<len(arr2):
+                if arr1[i]<arr2[j]:
+                    arr[k] = arr1[i]
+                    k+=1
+                    i+=1    
+                elif arr2[j]<arr1[i]:
+                    arr[k] = arr2[j]
+                    k+=1
+                    j+=1
+            # This part will make sure that if all elements of array 1 or 2 are inserted but some
+            # elements are left in another array.
+            # They are inserted in the same order as present in the remainig array.
+            # As both arrays are already sorted.
+            while i<len(arr1):
+                arr[k]=arr1[i]
+                k+=1
+                i+=1
+            while j<len(arr2):
+                arr[k]=arr2[j]
+                j+=1
+                k+=1
+obj = Solution()
+some = [49,9,4,9,121]
+obj.mergeSort(some,5)
+        
