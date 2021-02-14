cb42faa1c1dd9c3d618aa5a91d104cf55085bf00
Vansh <vansharora2929@gmail.com>
2021-02-14 21:10:03 +0530

    Better space and time

--- Arrays/max1s.py
+++ Arrays/max1s.py
@@ -1,15 +1,14 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        countList = []
+        tempCount = 0
         count = 0
         for i in nums:
             if i == 1:
-                count += 1
+                tempCount += 1
             else:
-                countList.append(count)
-                count = 0
-        if countList != []:
-            return max(countList)
-        else:
-            return 0
-        
+                if tempCount > count:
+                    count = tempCount
+                tempCount = 0
+        if tempCount > count:
+            count = tempCount
+        return count        
