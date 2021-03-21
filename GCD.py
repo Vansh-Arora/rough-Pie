4d1a95a9e1c5f29f535788aac994a98a1c236e73
Vansh <vansharora2929@gmail.com>
2021-03-22 01:25:47 +0530

    GCD

---
+++ GCD.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def gcd(self, A, B):
+        if B>A:
+            A,B = B,A
+        if B == 0:
+            return A
+        if A%B == 0:
+            return B
+        else:
+            return self.gcd(B,A%B)
