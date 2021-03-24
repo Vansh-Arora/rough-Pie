cced2c81f9ef63c9fd01c2454e0039fd0f529b89
Vansh <vansharora2929@gmail.com>
2021-03-25 02:38:19 +0530

    Final answer to find numbers of lengthK less thanC

--- NK.py
+++ NK.py
@@ -1,19 +1,65 @@
 class Solution:
     # @param A : list of integers
-    # @param B : integer
-    # @param C : integer
+    # @param B : integer length
+    # @param C : integer max + 1
     # @return an integer
     def solve(self, A, B, C):
-        min = 10**(B-1)
-        max = 9 * (11**(B-1))
-        ans = []
-        print(min,max,sep=' ')
-        for i in A:
-            if i in range(min,max+1) and i < C:
-                ans.append(i)
-        print(ans)
-        return ans
+        n = len(str(C))
+        array_size = len(A)
+
+        if array_size == 0:
+            ans = 0
         
+        elif n < B:
+            ans = 0
+        
+        elif n > B:
+            if 0 in A:
+                ans = (array_size - 1) * (array_size ** (B-1))
+            else:
+                ans = array_size ** B
+        
+        else:
+            dig_array = [int(i) for i in str(C)]
+            ans = 0
+            for i in range(n):
+                if i == 0:
+                    temp = 0
+                    j = 0
+                    if 0 in A:
+                        while(j < array_size and A[j] < dig_array[i]):
+                            temp += 1
+                            j += 1
+                        ans += (temp - 1) * (array_size ** (n-1))
+                        #print(ans)
+                    else:
+                        while(j < array_size and A[j] < dig_array[i]):
+                            temp += 1
+                            j += 1
+                        ans += (temp) * (array_size ** (n-1))
+                    #if ans == 0:
+                       # break
+                    if dig_array[i] not in A:
+                        break
+                else:
+                    temp = 0
+                    j = 0
+                    while(j < array_size and A[j] < dig_array[i]):
+                        temp += 1
+                        j += 1
+                    ans += temp *  (array_size**(n-i-1))
+                    if dig_array[i] not in A:
+                        break
+        if B == 1 and 0 in A:
+            ans += 1
+        return ans
+                    
 
 a = Solution()
-a.solve([0,1,5],1,2)
+
+ans = a.solve([0,1,2,5],2,21)
+print(ans)
+                        
+                        
+                        
+                        
