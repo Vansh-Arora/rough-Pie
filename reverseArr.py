fae30ee4b1b0de78afc6a2b73dd21671e3bdd977
Vansh <vansharora2929@gmail.com>
2021-03-25 22:24:24 +0530

    rev'

--- reverseArr.py
+++ reverseArr.py
@@ -1,10 +1,14 @@
+
 class Solution:
+    # @param A : list of integers
+    # Modify the array A which is passed by reference. 
+    # You do not need to return anything in this case. 
     def arrange(self, A):
         n = len(A)
-        a = [i for i in A]
         for i in range(n):
-            A[i] = a[a[i]]
-        return(A)
+            A[i] += (A[A[i]]%n)*n
+        for i in range(n):
+            A[i] = A[i]//n
 
 
 
@@ -12,4 +16,4 @@ A = Solution()
 x = input().split()
 x = [int(i) for i in x]
 a = A.arrange(x)
-print(a)
+print(x)
