e14a1db7f9a4c739d9eaecbe45faa938630ad271
Vansh <vansharora2929@gmail.com>
2021-03-27 01:18:30 +0530

    CELL NO

---
+++ cellNo.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def titleToNumber(self, A):
+        ans = 0
+        toPlaceValue = len(A)-1
+        for i in A:
+            ans += (ord(i) - 64) * (26 ** toPlaceValue)
+            toPlaceValue -= 1
+        return ans
+A = Solution()
+a = A.titleToNumber('BB')
+print(a)
