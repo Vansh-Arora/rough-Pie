bbeaea6b9076716762fcb8ea2c972ab77ebb4a00
Vansh <vansharora2929@gmail.com>
2021-04-08 04:56:37 +0530

    flip

--- flipMx1.py
+++ flipMx1.py
@@ -2,30 +2,46 @@ class Solution:
     # @param A : string
     # @return a list of integers
     def flip(self, A):
-        i = 0 
-        j = 0
-        ans  = []
-        count = 0
+        if '0' not in A:
+            return []
+        ans = [-1,-1]
         n = len(A)
-        while j < n:
-            if i == j and A[i] == 1:
-                i += 1
-                j += 1
-            elif A[j] == 0:
-                count += 1
-                ans[0] = i
-                ans[1] = j
-                j += 1
-            elif A[j] == 1:
-                if count > 0:
-                    count -= 1
+        start = 0
+        end = -1
+        count = 0
+        temp = 0
+        for i in range(n):
+            if A[i] == '0':
+                if temp < 0:
+                    start = i
+                    end = i
+                    temp = 0
                 else:
-                    count = 0
-                    i = j
-            j += 1
-        return ans
+                    end += 1  
+                temp += 1
+            
 
+            elif A[i] == '1':
+                if temp > count:
+                    count = temp
+                    ans[0] = start
+                    ans[1] = end
+                    end += 1
+                    temp -= 1
+                    
+                elif temp > 0:
+                    temp -= 1
+                    end += 1
+                elif temp == 0:
+                    temp -= 1
+
+        if temp > count:
+            count = temp
+            ans[0] = start
+            ans[1] = end
+        ans[0] += 1
+        ans[1] += 1
+        return ans
 A = Solution()
-print(A.flip('0011010000100000'))
-                
-                
+
+print(A.flip('01010'))
