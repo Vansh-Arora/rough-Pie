b295740a52f4b5b062807911ec9fc542badabee4
Vansh <vansharora2929@gmail.com>
2021-04-09 01:11:53 +0530

    done

--- array_multiples.py
+++ array_multiples.py
@@ -1,5 +1,9 @@
-def reduce(A):
+def reduce(A,var,j):
     if len(A) == 2:
+        var['a'+ str(j)] = [A[0],A[1]]
+        print([key for key in var.keys()])
+        print([val for val in var.values()])
+        return A
         return A
     n = len(A)
     if n % 2 != 0:
@@ -8,8 +12,16 @@ def reduce(A):
         last = n
     temp = []
     for i in range(0,last,2):
-        temp.append(A[i]*A[i+1])
+        var['a'+ str(j)] = [A[i],A[i+1]]
+        temp.append('a'+ str(j))
+
+        j += 1
     if last < n:
-        temp.append(A[last])
-    return reduce(temp)
-print(reduce([1,2,3,4,5]))
+
+        temp.append((A[last]))
+
+    
+    return reduce(temp,var,j)
+print(reduce(['R_1','R_2','R_3','R_4','R_5','R_6','R_7'],{},0))
+
+
