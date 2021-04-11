07133ff11d2b9efa4a3f516b812a445559a8b572
Vansh <vansharora2929@gmail.com>
2021-04-12 00:58:32 +0530

    Defined print functions

--- array_multiples.py
+++ array_multiples.py
@@ -1,10 +1,10 @@
+# Function to minimize array to 2 element
 def reduce(A,var,j):
     if len(A) == 2:
         var['a'+ str(j)] = [A[0],A[1]]
-        print([key for key in var.keys()])
-        print([val for val in var.values()])
-        return A
-        return A
+        print(A)
+        return var
+
     n = len(A)
     if n % 2 != 0:
         last = n-1 
@@ -17,11 +17,21 @@ def reduce(A,var,j):
 
         j += 1
     if last < n:
-
         temp.append((A[last]))
 
-    
     return reduce(temp,var,j)
-print(reduce(['R_1','R_2','R_3','R_4','R_5','R_6','R_7'],{},0))
+
+def reduceArr(A):
+    reduce(A,{},0)
+
+# Function to print intermediate Variables
+def print_var(var):
+    print([key for key in var.keys()])
+
+# Function to print intermediate pairs
+def print_pairs(var):
+    print([val for val in var.values()])
 
 
+# calling functions
+ans = reduceArr(['R_1','R_2','R_3','R_4','R_5','R_6','R_7'])
