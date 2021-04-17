0220e7c8c4e4630a119febc1206f600fa12cd0d9
Vansh <vansharora2929@gmail.com>
2021-04-18 03:13:19 +0530

    first node in cycle

---
+++ cycleNode.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @return the first node in the cycle in the linked list
+    def detectCycle(self, A):
+        if A == None:
+            return None
+        head = A
+        B = head.next
+        while B != None and B.next != None:
+            if head == B:
+                break
+            head = head.next
+            B = B.next.next
+        if head != B:
+            return None
+        head = A
+        while head != B:
+            head = head.next
+            B = B.next
+        return head
+
+
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @return the first node in the cycle in the linked list
+    def detectCycle(self, A):
+        if not A: return None
+        slow = fast = A
+        while slow and fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast: break
+        
+        if slow != fast: return None
+        slow = A
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+'''
