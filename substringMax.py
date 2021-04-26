fb1117eccc0b5194f3e66c1e466ff9f67cf5592b
Vansh <vansharora2929@gmail.com>
2021-04-26 13:00:00 +0530

    substringMax.py

--- substringMax.py
+++ substringMax.py
@@ -1,17 +1,21 @@
-st = 'abcabcbb'
-n = len(st)
-longest = 0
-for i in range(n):
-    temp = ''
-    for j in range(i,n):
-        if st[j] not in temp:
-            temp += st[j]
-        else:
-            if len(temp) > longest:
-                longest = len(temp)
+class Solution:
+    # @param A : string
+    # @return an integer
+    def lengthOfLongestSubstring(self, A):
+        st = A
+        n = len(st)
+        longest = 0
+        for i in range(n):
             temp = ''
-            break
-    if len(temp) != 0 and len(temp)>longest:
-        print(len(temp))
-        break
-print("longest {}".format(longest))
+            for j in range(i,n):
+                if st[j] not in temp:
+                    temp += st[j]
+                else:
+                    if len(temp) > longest:
+                        longest = len(temp)
+                    temp = ''
+                    break
+            if len(temp) != 0 and len(temp)>longest:
+                return (len(temp))
+                
+        return longest
