bcfbeb691493d3f3759fd0871f24104bb91be933
Vansh <vansharora2929@gmail.com>
2021-05-02 10:27:18 +0530

    max_abs_diff.py

--- max_abs_diff.py
+++ max_abs_diff.py
@@ -1,4 +1,15 @@
-n= 4
-for i in range(1,n):
-    for j in range(0,i):
-        print(i,j,sep=" ")
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def maxArr(self, A):
+        n = len(A)
+        ans  = 0
+        for i in range(1,n):
+            for j in range(i):
+                print(i,j,sep=" ")
+                temp = abs(A[i] - A[j]) + abs(i - j)
+                if temp > ans:
+                    ans = temp
+        return ans
+                
+                
