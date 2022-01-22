d1800f55719575732ebbb3708ee30382e200e6ff
Vansh <vansharora2929@gmail.com>
2022-01-22 14:48:39 +0530

    Moved Files:

--- scribble_kiddies/array_subsets.py
+++
@@ -1,7 +0,0 @@
-def subsets(arr,n,i,osf):
-    if i == n:
-        print ('[' + osf + ']')
-        return
-    subsets(arr,n,i+1,osf)
-    subsets(arr,n,i+1,osf+str(arr[i])+',')
-subsets([1,2,3],3,0,'')   
--- scribble_kiddies/revString.py
+++
@@ -1,8 +0,0 @@
-def rev(str,n):
-    if n==0:
-        print(str[n])
-        return
-    print(str[n])
-    rev(str,n-1)
-rev("BAC",2)
-        
