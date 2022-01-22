7cdd5261193f2f73ae976fe6d9e2208b099a062a
Vansh <vansharora2929@gmail.com>
2022-01-22 14:38:19 +0530

    subsets

---
+++ scribble_kiddies/backTrack/subsets.py
@@ -0,0 +1,13 @@
+class subsets:
+    def generate(self,A,ANS,i,n):
+        if i == n:
+            print(ANS)
+            return
+        ANS.append(A[i])
+        self.generate(A,ANS,i+1,n)
+        #ANS.pop()
+        self.generate(A,ANS,i+1,n)
+
+A = subsets()
+A.generate([1,2,3],[],0,3)
+        
