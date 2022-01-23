2a070e9d18d5420c757e906ea80f75ae27e854d7
Vansh <vansharora2929@gmail.com>
2022-01-23 14:09:06 +0530

    Queue

---
+++ scribble_kiddies/Queue/Queue.py
@@ -0,0 +1,40 @@
+class Queue:
+    def __init__(self):
+        self.q = []
+        self.front = -1
+        self.rear = -1
+
+    def isEmpty(self):
+        if self.rear == -1:
+            return True
+        return False
+    
+    def enQueue(self,data):
+        if self.front == -1:
+            self.front += 1
+        self.rear += 1
+        self.q.append(data)
+    
+    def deQueue(self):
+        if self.front == -1 or self.front > self.rear:
+            return None
+        self.front += 1
+        return self.q[self.front - 1]
+    
+    def display(self):
+        i = self.front
+        while i<=self.rear:
+            print(self.q[i])
+            i += 1
+
+    def fillQueue(self):
+        val = input()
+        while val != '-1':
+            self.enQueue(val)
+            val = input()
+    
+if __name__ == "__main__":
+    Que = Queue()
+    Que.fillQueue()
+    Que.display()
+    print(Que.deQueue())
