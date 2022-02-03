dfe68a55e5a7dddfcdbe9db0dc4914dca2470b28
Vansh <vansharora2929@gmail.com>
2022-02-03 22:29:21 +0530

    inverted Pyramid

---
+++ scribble_kiddies/patterns/invertedPyramid.py
@@ -0,0 +1,11 @@
+def create(n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if j <= (n-i):
+                print(" ",end = "")
+            else:
+                print("*",end = "")
+        print()
+
+
+create(5)
