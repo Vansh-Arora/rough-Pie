b15eaef6b373db05b0a432df9b91d48674ded2df
Vansh <vansharora2929@gmail.com>
2022-02-07 01:54:14 +0530

    Adjacency List

--- scribble_kiddies/graph/adjacencyList.py
+++ scribble_kiddies/graph/adjacencyList.py
@@ -15,8 +15,27 @@ class singlyLinkedList:
             self.tail.next = node
         self.tail = node
 
-def createList(nodes):
+def createList(nodes,edges):
     adjList = [None for i in range(nodes)]
     for i in range(nodes):
          adjList[i] = singlyLinkedList()
+    for i in range(edges):
+        node1 = int(input("Enter node A data: "))
+        node2 = int(input("Enter node B data: "))
+        adjList[node1].addNode(node2)
+        adjList[node2].addNode(node1)
+    return adjList
     
+def printList(adjList,nodes):
+    for i in range(nodes):
+
+        print(i,end="->")
+        curr = adjList[i].head
+        while curr:
+            print(curr.data,end="->")
+            curr = curr.next
+        print("X")
+
+if __name__ == "__main__":
+    adjList = createList(7,7)
+    printList(adjList,7)
