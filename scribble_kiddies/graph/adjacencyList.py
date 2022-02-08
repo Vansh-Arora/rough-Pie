7bba4610a9ac713747ad508351ea3a81e2cc2e84
Vansh <vansharora2929@gmail.com>
2022-02-08 16:37:35 +0530

    :Added Queue:

--- scribble_kiddies/graph/adjacencyList.py
+++ scribble_kiddies/graph/adjacencyList.py
@@ -16,7 +16,40 @@ class singlyLinkedList:
         else:
             self.tail.next = node
         self.tail = node
+class Queue:
+    def __init__(self):
+        self.q = []
+        self.front = -1
+        self.rear = -1
 
+    def isEmpty(self):
+        if self.rear == -1 or self.front>self.rear:
+            return True
+        return False
+    
+    def enQueue(self,data):
+        if self.front == -1:
+            self.front += 1
+        self.rear += 1
+        self.q.append(data)
+    
+    def deQueue(self):
+        if self.front == -1 or self.front > self.rear:
+            return None
+        self.front += 1
+        return self.q[self.front - 1]
+    
+    def display(self):
+        i = self.front
+        while i<=self.rear:
+            print(self.q[i])
+            i += 1
+
+    def fillQueue(self):
+        val = input()
+        while val != '-1':
+            self.enQueue(val)
+            val = input()
 
 ## Create adjacency list
 def createList(nodes,edges):
@@ -39,6 +72,11 @@ def printList(adjList,nodes):
             print(curr.data,end="->")
             curr = curr.next
         print("X")
+
+# Breadth First Search
+#sdef BFS(adjList):
+    
+
 
 if __name__ == "__main__":
     nodes = int(input("Enter Number of Nodes: "))
