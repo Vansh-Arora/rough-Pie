7020b7db75b5ed5a3acb6a29219943292f7bf3b7
Vansh <vansharora2929@gmail.com>
2022-02-09 19:28:27 +0530

    topological sort

--- scribble_kiddies/graph/D_adjacencyList.py
+++ scribble_kiddies/graph/D_adjacencyList.py
@@ -15,9 +15,45 @@ class singlyLinkedList:
         else:
             self.tail.next = node
         self.tail = node
+# Queue
+class Queue:
+    def __init__(self):
+        self.q = []
+        self.front = -1
+        self.rear = -1
+
+    def isEmpty(self):
+        if self.rear == -1 or self.front>self.rear:
+            return True
+        return False
+    
+    def enQueue(self,data):
+        if self.front == -1:
+            self.front += 1
+        self.rear += 1
+        self.q.append(data)
+    
+    def deQueue(self):
+        if self.front == -1 or self.front > self.rear:
+            return None
+        self.front += 1
+        return self.q[self.front - 1]
+    
+    def display(self):
+        i = self.front
+        while i<=self.rear:
+            print(self.q[i])
+            i += 1
+
+    def fillQueue(self):
+        val = input()
+        while val != '-1':
+            self.enQueue(val)
+            val = input()
 
 ## Create adjacency list
 def createList(nodes,edges):
+    global indeg
     adjList = [None for i in range(nodes)]
     for i in range(nodes):
          adjList[i] = singlyLinkedList()
@@ -25,6 +61,7 @@ def createList(nodes,edges):
         node1 = int(input("Enter start node data: "))
         node2 = int(input("Enter end node data: "))
         adjList[node1].addNode(node2)
+        indeg[node2] += 1
     return adjList
 
 ## Print List
@@ -36,11 +73,31 @@ def printList(adjList,nodes):
             print(curr.data,end="->")
             curr = curr.next
         print("X")
+def TopologicalSort(adjList):
+    count = 0
+    Q = Queue()
+    for i in range(len(indeg)):
+        if indeg[i] == 0:
+            Q.enQueue(i)
+    while not Q.isEmpty():
+        currentNode = Q.deQueue()
+        print(currentNode,end="->")
+        curr = adjList[currentNode].head
+        while curr:
+            indeg[curr.data] -= 1
+            if indeg[curr.data] == 0:
+                Q.enQueue(curr.data)
+            curr = curr.next
 
+            
 
 if __name__ == "__main__":
     nodes = int(input("Enter Number of Nodes: "))
     edges = int(input("Enter Number of edges: "))
+    indeg = [0 for i in range(nodes)]
+
     adjList = createList(nodes,edges)
     printList(adjList,7)
     print()
+    print(indeg)
+    TopologicalSort(adjList)
