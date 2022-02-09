06f41485f23ab10c943ec67a2e0e1289bfa852ea
Vansh <vansharora2929@gmail.com>
2022-02-09 17:46:41 +0530

    Template

---
+++ scribble_kiddies/graph/D_adjacencyList.py
@@ -0,0 +1,29 @@
+# Linked List
+class singlyLinkedListNode:
+    def __init__(self,data):
+        self.next = None
+        self.data = data
+class singlyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def addNode(self,data):
+        node = singlyLinkedListNode(data)
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+## Create adjacency list
+def createList(nodes,edges):
+    adjList = [None for i in range(nodes)]
+    for i in range(nodes):
+         adjList[i] = singlyLinkedList()
+    for i in range(edges):
+        node1 = int(input("Enter node A data: "))
+        node2 = int(input("Enter node B data: "))
+        adjList[node1].addNode(node2)
+        adjList[node2].addNode(node1)
+    return adjList
