64f5ce3133b31f38147cb34c698b0b10c6d954e7
Vansh <vansharora2929@gmail.com>
2022-02-14 13:00:00 +0530

    DFS

---
+++ scribble_kiddies/GFG/Graphs/DFS.py
@@ -0,0 +1,22 @@
+def DFShelper(V,adj,node,ans):
+        V[node] = 1
+        ans.append(node)
+        for i in adj[node]:
+            if V[i] == 0:
+                DFShelper(V,adj,i)
+
+def DFS(adj,V):
+    visi = [0 for i in range(V)]
+    ans = []
+    count = 0
+    for i in range(V):
+        if visi[i] == 0:
+            count += 1
+            DFShelper(visi,adj,i,ans)
+    #print("Connected Components = {}".format(count))
+
+
+
+# [[1, 2], [0, 3, 4, 5, 6], [0, 6], [1], [1], [1], [1, 2]]
+adjList = [[1, 2], [0, 3, 4, 5, 6], [0, 6], [1], [1], [1], [1, 2], [8, 9], [7], [7], [11], [10]]
+DFS(adjList,12)
