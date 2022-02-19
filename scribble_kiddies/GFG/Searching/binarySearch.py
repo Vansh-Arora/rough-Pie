79244a0c1ef12e94732b98879bef88da12b5f515
Vansh <vansharora2929@gmail.com>
2022-02-19 23:10:53 +0530

    binarySearch.py

--- scribble_kiddies/GFG/Searching/binarySearch.py
+++ scribble_kiddies/GFG/Searching/binarySearch.py
@@ -13,8 +13,20 @@ def binarySearch(A,n):
             start = mid + 1
     return -1
 
-
+# Recursive Approcah
+def binSearch(A,k,start,end):
+    if start > end:
+        return -1
+    mid = (start + end)//2
+    if A[mid] == k:
+        return mid
+    if A[mid] > k:
+        return binSearch(A,k,start,mid-1)
+    else:
+        return binSearch(A,k,mid+1,end)
 
 if __name__ == "__main__":
     A = [1,2,6,78,90]
+    a = [1,2,3,4,5]
     print(binarySearch(A,6))
+    print(binSearch(a,4,0,4))
