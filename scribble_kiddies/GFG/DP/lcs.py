86b4bbb7a3115d7c2768439ba4d751ae1ec420a1
Vansh <vansharora2929@gmail.com>
2022-02-22 13:00:00 +0530

    LCS

--- scribble_kiddies/GFG/DP/lcs.py
+++ scribble_kiddies/GFG/DP/lcs.py
@@ -9,6 +9,8 @@ def lcs(s1,s2,m,n):
         return max(lcs(s1,s2,m-1,n),lcs(s1,s2,m,n-1))
 '''
 ## Using DP
+
+## Mempoization
 m = 4
 n = 3
 S = [[-1 for i in range(n+1)] for j in range(m+1)]
@@ -23,6 +25,24 @@ def lcs(s1,s2,m,n):
         S[m][n] =  max(lcs(s1,s2,m-1,n),lcs(s1,s2,m,n-1))
     return S[m][n]
 
+# Tabularization
+def tlcs(s1,s2):
+    m = len(s1)
+    n = len(s2)
+    table = [[-1 for i in range(n+1)] for j in range(m+1)]
+    for i in range(m+1):
+        table[i][0] = 0
+    for j in range(n+1):
+        table[0][j] = 0
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if s1[i-1] == s2[j-1]:
+                table[i][j] = 1 + table[i-1][j-1]
+            else:
+                table[i][j] = max(table[i][j-1],table[i-1][j])
+    return table[m][n]
+
+
 s1 = "BACD"
-s2 = "BCD"
-print(lcs(s1,s2,m,n))
+s2 = "BCDA"
+print(tlcs(s1,s2))
