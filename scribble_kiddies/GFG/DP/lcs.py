a2513eae8578250014cafb22ee9db67593c33bc5
Vansh <vansharora2929@gmail.com>
2022-02-23 19:40:02 +0530

    lcs.py

---
+++ scribble_kiddies/GFG/DP/lcs.py
@@ -0,0 +1,14 @@
+# Recursive Solution
+''''
+def lcs(s1,s2,m,n):
+    if m == 0 or n == 0:
+        return 0
+    if s1[m-1] == s2[n-1]:
+        return 1 + lcs(s1,s2,m-1,n-1)
+    else:
+        return max(lcs(s1,s2,m-1,n),lcs(s1,s2,m,n-1))
+'''
+
+s1 = "BACD"
+s2 = "BCD"
+print(lcs(s1,s2,m,n))
