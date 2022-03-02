6b3fe2db8d2029b750db113f91c7fed9e67440ce
Vansh <vansharora2929@gmail.com>
2022-03-02 15:09:09 +0530

    rope_cutting.py

--- scribble_kiddies/recursion/rope_cutting.py
+++ scribble_kiddies/recursion/rope_cutting.py
@@ -1,1 +1,11 @@
-def cut():
+def cut(n,A,B,C,S,P,ans):
+    if S>n:
+        return ans
+    if S==n:
+        ans.append(P)
+        return ans
+    cut(n,A,B,C,S+A,P+1,ans)
+    cut(n,A,B,C,S+B,P+1,ans)
+    cut(n,A,B,C,S+C,P+1,ans)
+    return max(ans)
+print(cut(5,4,2,6,0,0,[-1]))
