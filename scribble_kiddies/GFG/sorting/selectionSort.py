b5c3eb1b182c4fc72d36b27b6265af455ea9e614
Vansh <vansharora2929@gmail.com>
2022-03-09 01:00:26 +0530

    selectionSort.py

--- scribble_kiddies/GFG/sorting/selectionSort.py
+++ scribble_kiddies/GFG/sorting/selectionSort.py
@@ -1,7 +1,14 @@
-def sort():
+def sort(A):
+    n = len(A)
     for i in range(n):
         min = i
         for j in range(i+1,n):
             if A[j] < A[min]:
                 min = j
         A[i],A[min] = A[min],A[i]
+
+
+
+A = [2,1,4,7,5]
+sort(A)
+print(A)
