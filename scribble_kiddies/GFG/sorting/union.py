10d2c2a54236b904938b06cfb73de5f37e21e391
Vansh <vansharora2929@gmail.com>
2022-03-15 00:23:56 +0530

    union.py

--- scribble_kiddies/GFG/sorting/union.py
+++ scribble_kiddies/GFG/sorting/union.py
@@ -7,18 +7,32 @@ def union(A,B):
         if i != 0 and A[i] == A[i-1]:
             i += 1
             continue
-        if j != 0 and A[j] == A[j-1]:
+        if j != 0 and B[j] == B[j-1]:
             j += 1
             continue
-        if A[i] < A[j]:
+        if A[i] < B[j]:
             print(A[i],end = " ")
             i += 1
-        elif A[j] <  A[i]:
-            print(A[j],end = " ")
+        elif B[j] <  A[i]:
+            print(B[j],end = " ")
             j += 1
         else:
-            print(A[i])
+            print(A[i],end = " ")
             i += 1
             j += 1
+    while i < m:
+        if i != 0 and A[i] == A[i-1]:
+            i += 1
+            continue
+    while j < n:
+        if j != 0 and B[j] == B[j-1]:
+            j += 1
+            continue
+
+
 
             
+if __name__ == "__main__":
+    A = [1,1,3,5,6,7,8,9]
+    B = [6,6,7,8]
+    union(A,B)
