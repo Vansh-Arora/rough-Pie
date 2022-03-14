49177fdc07090fa6134ba10c283e21189317c0ed
Vansh <vansharora2929@gmail.com>
2022-03-14 19:47:10 +0530

    minor error

--- scribble_kiddies/GFG/sorting/intersection.py
+++ scribble_kiddies/GFG/sorting/intersection.py
@@ -5,6 +5,7 @@ def intersection(A,B):
     n = len(B)
     while i < m and j < n:
         if i > 0 and A[i] == A[i-1]:
+            i += 1
             continue
         if A[i] > B[j]:
             j += 1
@@ -16,6 +17,6 @@ def intersection(A,B):
             j += 1
             
 if __name__ == "__main__":
-    A = [1,3,5,6,7,8]
+    A = [1,1,3,5,6,7,8]
     B = [6,6,7,8]
     intersection(A,B)
