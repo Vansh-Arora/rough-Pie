35c695614ae10cf2a3b68057bbe48bd6a879cfd0
Vansh <vansharora2929@gmail.com>
2022-03-14 20:48:25 +0530

    Union of 2 arrays

--- scribble_kiddies/GFG/sorting/union.py
+++ scribble_kiddies/GFG/sorting/union.py
@@ -10,3 +10,15 @@ def union(A,B):
         if j != 0 and A[j] == A[j-1]:
             j += 1
             continue
+        if A[i] < A[j]:
+            print(A[i],end = " ")
+            i += 1
+        elif A[j] <  A[i]:
+            print(A[j],end = " ")
+            j += 1
+        else:
+            print(A[i])
+            i += 1
+            j += 1
+
+            
