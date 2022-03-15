9a6fd883ceacfb780b2d0f70dff36d73562233d9
Vansh <vansharora2929@gmail.com>
2022-03-15 15:31:17 +0530

    count inversions in array

---
+++ scribble_kiddies/GFG/sorting/countInversion.py
@@ -0,0 +1,27 @@
+def mergeNcount(arr,l,m,r):
+    left = arr[l:m+1]
+    right = arr[m+1:r+1]
+    i = 0
+    j = 0
+    k = l
+    n1 = len(left)
+    n2 = len(right)
+    while i < n1 and j < n2:
+        if left[i] < right[j]:
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        elif right[j] < left[i]:
+            arr[k] = right[j]
+            k += 1
+            res += (n1 - i)
+            j += 1
+
+def divide(arr,l,r):
+    res = 0
+    if l < r:
+        m = (l+r) // 2
+        res += divide(arr,l,m)
+        res += divide(arr,m+1,r)
+        res += mergeNcount(arr,l,m,r)
+    return res
