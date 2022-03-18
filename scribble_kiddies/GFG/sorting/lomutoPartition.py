5fa6f51721d19c7e78a01feae4bcafac54415a52
Vansh <vansharora2929@gmail.com>
2022-03-18 13:00:00 +0530

    lomuto partition

---
+++ scribble_kiddies/GFG/sorting/lomutoPartition.py
@@ -0,0 +1,9 @@
+def lomuto(A,l,r):
+    p = r
+    j = l-1
+    for i in range(r-1):
+        if A[i] < A[p]:
+            j += 1
+            A[i],A[j] = A[j],A[i]
+    A[j+1],A[p] = A[p],A[j+1]
+    print(A)
