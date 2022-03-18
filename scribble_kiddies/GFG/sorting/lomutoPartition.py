c671c3dcae319c9f153fdcd65edc3a54c1a368e6
Vansh <vansharora2929@gmail.com>
2022-03-19 01:40:11 +0530

    Tested

--- scribble_kiddies/GFG/sorting/lomutoPartition.py
+++ scribble_kiddies/GFG/sorting/lomutoPartition.py
@@ -1,9 +1,11 @@
 def lomuto(A,l,r):
     p = r
     j = l-1
-    for i in range(r-1):
+    for i in range(r):
         if A[i] <= A[p]:
             j += 1
             A[i],A[j] = A[j],A[i]
     A[j+1],A[p] = A[p],A[j+1]
     print(A)
+
+lomuto([2,3,4,13,1,3,6,5,4,7],0,9)
