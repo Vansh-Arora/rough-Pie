2654b03a6140d4d438e1746dba5aebdb20e78076
Vansh <vansharora2929@gmail.com>
2022-03-19 17:14:27 +0530

    Added explaination

--- scribble_kiddies/GFG/sorting/lomutoPartition.py
+++ scribble_kiddies/GFG/sorting/lomutoPartition.py
@@ -1,11 +1,14 @@
+# Lomuto partition ensures that pivot is placed at its right position
+# It places all the elements smaller than or equal to pivot on left of pivot
+# all elemnts greater than or equal to pivot on right of pivot.
 def lomuto(A,l,r):
     p = r
     j = l-1
     for i in range(r):
-        if A[i] <= A[p]:
+        if A[i] < A[p]:
             j += 1
             A[i],A[j] = A[j],A[i]
     A[j+1],A[p] = A[p],A[j+1]
     print(A)
 if __name__ == "__main__":
-    lomuto([2,3,4,13,1,3,6,5,4,7],0,9)
+    lomuto([2,3,4,13,1,3,6,5,4],0,8)
