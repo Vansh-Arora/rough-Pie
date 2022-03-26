1990f124bafdacf9b8feb165c1d8a4df6c3d91c7
Vansh <vansharora2929@gmail.com>
2022-03-27 02:27:58 +0530

    activitySelection.py: main

--- scribble_kiddies/GFG/Greedy/activitySelection.py
+++ scribble_kiddies/GFG/Greedy/activitySelection.py
@@ -13,8 +13,17 @@ def qSort(A,l,r):
         qSort(A,l,p-1)
         qSort(A,p+1,r)
 
+def selector(A):
+    qSort(A,0,len(A)-1)
+    n = len(A)
+    ans = []
+    ans.append(A[0])
+    for i in range(1,n):
+        if A[i][0] >= ans[-1][1]:
+            ans.append(A[i])    
+    return ans
+
 if __name__ == "__main__":
     A = [[0,1],[1,3],[2,5],[1,2]]
-    qSort(A,0,3)
-    print(A)
-    
+    ans = selector(A)
+    print(ans)
