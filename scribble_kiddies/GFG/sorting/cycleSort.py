32bd22dd8c5b7c2fe3b015d9bd4a6cc46dce22c0
Vansh <vansharora2929@gmail.com>
2022-03-29 00:24:27 +0530

    cycleSort.py

--- scribble_kiddies/GFG/sorting/cycleSort.py
+++ scribble_kiddies/GFG/sorting/cycleSort.py
@@ -1,13 +1,14 @@
 def sort(A):
     n = len(A)
-    for curr in range(n-1):
-        item = A[curr]
-        pos = curr
+    for curr in range(n-1):  # place all elements at their respective places
+        item = A[curr]         # taking each element iteratively
+        pos = curr             # cycle sort ensures that previous elemnts are sorted so we check no. of elements
+                               # smaller after given place and shift the element by those number of places
         for i in range(curr+1,n):
             if A[i] < item:
                 pos += 1
-        item,A[pos] = A[pos],item
-        while curr != pos:
+        item,A[pos] = A[pos],item   # Now the item becomes the elemnt which has been taken out from the array
+        while curr != pos:          # We repeat the above cycle till the element at curr position is found
             pos = curr
             for i in range(curr+1,n):
                 if A[i] < item:
