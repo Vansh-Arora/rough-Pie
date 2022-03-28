defcedda14391b3f3eedc4bab20ba74591120f02
Vansh <vansharora2929@gmail.com>
2022-03-28 18:06:23 +0530

    minDiff.py: main

--- scribble_kiddies/GFG/sorting/minDiff.py
+++ scribble_kiddies/GFG/sorting/minDiff.py
@@ -5,4 +5,8 @@ def getMinDiff(A):
     for i in range(1,n):
         mini = min(mini,A[i]-A[i-1])
     return mini
-        
+    
+
+if __name__ == "__main__":
+    A = [23,56,87,99,97.6,98,76]
+    print(getMinDiff(A))
