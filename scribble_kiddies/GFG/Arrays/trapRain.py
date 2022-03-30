50c33af562cadabe1d3ea9b1ca3511c5169fa9aa
Vansh <vansharora2929@gmail.com>
2022-03-30 13:00:00 +0530

    trapRain.py

---
+++ scribble_kiddies/GFG/Arrays/trapRain.py
@@ -0,0 +1,20 @@
+def trap(A):
+    n = len(A)
+    lmax = [-1 for i in range(n)]
+    rmax = [-1 for i in range(n)]
+    lmax[0] = A[0]
+    for i in range(1,n):
+        lmax[i] = max(lmax[i-1],A[i])
+    rmax[n-1] = A[n-1]
+    for i in range(n-2,-1,-1):
+        rmax[i] = max(rmax[i+1],A[i])
+    ans = 0
+    for i in range(n):
+        ans += (min(lmax[i],rmax[i]) - A[i])
+    
+    return ans
+
+
+A = [5,0,6,2,3]
+ans = trap(A)
+print(ans)
