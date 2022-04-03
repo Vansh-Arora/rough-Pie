593456fbd78e01fca9d3e0c841ecc044b4a6f23c
Vansh <vansharora2929@gmail.com>
2022-04-03 13:00:00 +0530

    consecutive1s.py

---
+++ scribble_kiddies/GFG/Arrays/consecutive1s.py
@@ -0,0 +1,15 @@
+def get(A):
+    res = float("-inf")
+    count = 0
+    for i in A:
+        if i == 1:
+            count += 1
+            if count > res:
+                res = count
+        else:
+            count = 0
+    return res
+
+A = [0,0,1,1,1,1,0,0,1,1,0,0,1,1,1,0,1]
+ans = get(A)
+print(ans)
