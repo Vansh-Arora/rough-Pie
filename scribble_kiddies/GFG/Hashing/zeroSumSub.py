7fecedabd45a34e7c29f8bf59429ebd66e6ea679
Vansh <vansharora2929@gmail.com>
2022-04-13 03:21:57 +0530

    Fixed 0 element problem

--- scribble_kiddies/GFG/Hashing/zeroSumSub.py
+++ scribble_kiddies/GFG/Hashing/zeroSumSub.py
@@ -3,13 +3,14 @@ def getSub(A):
     sum = 0
     for i in A:
         sum += i
+        if sum == 0:
+            return True
         if sum in sub:
             return True
-        else:
-            sub[sum] = 0
+        sub[sum] = 0
     return False
 
 if __name__ == "__main__":
-    ans = getSub([1,2,4,5,6,7,8,1,2,3,4,5,6,7,8,-9,-8,0])
+    ans = getSub([0,1,2])
     print(ans)
         
