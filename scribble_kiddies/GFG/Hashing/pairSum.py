da5f4522b5ae93a372bcfe20dae0118cfba07455
Vansh <vansharora2929@gmail.com>
2022-04-13 02:55:59 +0530

    main

--- scribble_kiddies/GFG/Hashing/pairSum.py
+++ scribble_kiddies/GFG/Hashing/pairSum.py
@@ -6,5 +6,8 @@ def getPair(A,S):
         else:
             if i not in table:
                 table[i] = 0
-        
-        
+    return False
+
+if __name__ == "__main__":
+    ans = getPair([1,2,4,5,6,7,8,1,2,3,4,5,6,7,8],17)
+    print(ans)
