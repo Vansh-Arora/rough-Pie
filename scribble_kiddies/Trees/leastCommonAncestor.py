95f6eb8b58c3e658287f9d6618c1515800b790bc
Vansh <vansharora2929@gmail.com>
2022-04-15 01:33:31 +0530

    least common ancestor

---
+++ scribble_kiddies/Trees/leastCommonAncestor.py
@@ -0,0 +1,13 @@
+def lca(root,n1,n2):
+    if root == None:
+        return None
+    if root.data == n1 or root.data == n2:
+        return root
+    lefty = lca(root.left,n1,n2)
+    righty = lca(root.right,n1,n2)
+    if lefty != None and righty != None:
+        return root
+    if lefty != None:
+        return lefty
+    else:
+        return righty
