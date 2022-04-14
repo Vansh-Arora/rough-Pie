708d243ded1a9a93d1bc2af5ed4d5fd25f817deb
Vansh <vansharora2929@gmail.com>
2022-04-14 23:55:12 +0530

    Indendation

--- scribble_kiddies/GFG/Hashing/anagram.py
+++ scribble_kiddies/GFG/Hashing/anagram.py
@@ -1,20 +1,12 @@
-class Solution:
-    def Anagrams(self, words, n):
-        '''
-        words: list of word
-        n:      no of words
-        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
-        '''
-        
-        #code here
-        dis = {}
-        for i in words:
-            temp = "".join(sorted(i))
-			if temp not in dis:
-				dis[temp] = [i]
-			else:
-				dis[temp].append(i)
-		ans = []
-		for i in dis:
-			ans.append(dis[i])
-		return ans
+def Anagrams(words):
+	dis = {}
+	for i in words:
+		temp = "".join(sorted(i))
+		if temp not in dis:
+			dis[temp] = [i]
+		else:
+			dis[temp].append(i)
+	ans = []
+	for i in dis:
+		ans.append(dis[i])
+	return ans
