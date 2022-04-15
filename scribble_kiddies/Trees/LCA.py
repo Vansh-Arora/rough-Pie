901b1a3c5d1905b6a741607f02d04ac7b4538898
Vansh <vansharora2929@gmail.com>
2022-04-16 03:16:58 +0530

    LCA

---
+++ scribble_kiddies/Trees/LCA.py
@@ -0,0 +1,49 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.left = None
+#		self.right = None
+
+class Solution:
+	# @param A : root node of tree
+	# @param B : integer
+	# @param C : integer
+	# @return an integer
+	def lca(self, A, B, C):
+        pathB = []
+        cur = A
+        flag = 0
+        while cur or len(pathB) > 0:
+            while cur:
+                pathB.append(cur.val)
+                if pathB[-1].val == B:
+                    flag = 1
+                    break
+                cur = cur.left
+            if flag: break
+            cur = pathB.pop()
+            cur = cur.right()
+        pathC = []
+        cur = A
+        flag = 0
+        while cur or len(pathC) > 0:
+            while cur:
+                pathC.append(cur.val)
+                if pathC[-1].val == C:
+                    flag = 1
+                    break
+                cur = cur.left
+            if flag: break
+            cur = pathC.pop()
+            cur = cur.right()
+        
+        n = min(len(pathB),len(pathC))
+        for i in range(n):
+            if pathB[i] != pathC[i]:
+                return pathB[i-1]
+        
+
+
+
+ 
