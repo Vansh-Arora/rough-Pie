d0e77f0da2eea7c3191d3c09b36723a8967e1a40
Vansh <vansharora2929@gmail.com>
2022-04-18 00:34:22 +0530

    spiral

--- scribble_kiddies/GFG/Arrays/spiral1.py
+++ scribble_kiddies/GFG/Arrays/spiral1.py
@@ -1,2 +1,34 @@
-M = [[-1 for i in range(3)] for i in range(3)]
-print(M)
+class Solution:
+    # @param A : tuple of list of integers
+    # @return a list of integers
+    def spiralOrder(self, A):
+        R = len(A) - 1
+        C = len(A[0]) - 1
+        i = 0
+        j = 0
+        ans = []
+        
+        while i <= R and j <= C:
+            for k in range(j,C+1):
+                ans.append(A[i][k])
+            i += 1
+            if i > R:
+                break
+            
+            for k in range(i,R+1):
+                ans.append(A[k][C])
+            C -= 1
+            if j > C:
+                break
+            
+            for k in range(C,j-1,-1):
+                ans.append(A[R][k])
+            R -= 1
+            if i > R:
+                break
+            for k in range(R,i-1,-1):
+                ans.append(A[k][j])
+            j += 1
+            if j > C:
+                break
+        return ans
