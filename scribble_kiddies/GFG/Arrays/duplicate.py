ca15b65eb499ba8ec2e5b699dfc52e641768540a
Vansh <vansharora2929@gmail.com>
2022-04-21 00:44:12 +0530

    RENOVED EXTRA VARIABLE

--- scribble_kiddies/GFG/Arrays/duplicate.py
+++ scribble_kiddies/GFG/Arrays/duplicate.py
@@ -2,7 +2,6 @@ class Solution:
     # @param A : tuple of integers
     # @return an integer
     def repeatedNumber(self, A):
-        n = len(A)
         dup = {}
         for i in A:
             if i in dup:
