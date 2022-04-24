429e00aaf17c2dbde30a397c66b8317fbfaecba5
Vansh <vansharora2929@gmail.com>
2022-04-25 04:47:06 +0530

    27

--- scribble_kiddies/Strings/RomansToInts.py
+++ scribble_kiddies/Strings/RomansToInts.py
@@ -1,17 +1,15 @@
-class Solution:
-	# @param A : string
-	# @return an integer
-	def romanToInt(self, A):
-        ans = 0
-        ROMANS = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
-        n = len(A)
-        for i in range(n-1):
-            if ROMANS[A[i]] < ROMANS[A[i+1]]:
-                ans -= ROMANS[A[i]]
-            else:
-                ans += ROMANS[A[i]]
-        ans += ROMANS[A[n-1]]
-        return ans
+def romanToInt(A):
+    ans = 0
+    ROMANS = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
+    n = len(A)
+    for i in range(n-1):
+        if ROMANS[A[i]] < ROMANS[A[i+1]]:
+            ans -= ROMANS[A[i]]
+        else:
+            ans += ROMANS[A[i]]
+    ans += ROMANS[A[n-1]]
+    return ans
 
 
 
+print(romanToInt('XXVII'))
